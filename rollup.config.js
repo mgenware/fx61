534eb7291c4e944a12d10939177cfefbadf91ae6
Mgen <m@mgenware.com>
2020-04-23 23:17:25 +0800

    Use package.json name as output name

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,8 @@ import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import typescript from 'rollup-plugin-typescript2';
 import { terser } from 'rollup-plugin-terser';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { name: pkgName } = require('./package.json');
 
 const isProd = process.env.NODE_ENV == 'production';
 
@@ -21,7 +23,7 @@ const tasks = [
   {
     input: 'src/main.ts',
     output: {
-      name: 'fx61',
+      name: pkgName,
       file: 'dist/main.js',
       format: 'umd',
       exports: 'named',
